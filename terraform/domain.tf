24b3fd5b02d3f57ebcdd6d0d412121e9b29c6b27
Aaron Kanzer <aaronkanzer@dhcp-10-29-252-95.dyn.MIT.EDU>
2024-02-07 10:07:40 -0500

    more commenting out to clean up TF

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -10,6 +10,7 @@ resource "aws_route53_record" "gui" {
   records = ["75.2.60.5"] # Netlify's load balancer,  which will proxy to our app -- https://docs.netlify.com/domains-https/custom-domains/configure-external-dns/#configure-an-apex-domain
 }
 
+# Aaron -- ended up unsuccessful -- had to generate staging manually
 # resource "aws_route53_record" "gui-staging" {
 #   zone_id = aws_route53_zone.linc-brain-mit.zone_id
 #   name    = "gui-staging"
@@ -27,32 +28,32 @@ resource "aws_acm_certificate" "cert" {
   ]
 }
 
-
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for domain_validation_option in aws_acm_certificate.cert.domain_validation_options : domain_validation_option.domain_name => {
-      name   = domain_validation_option.resource_record_name
-      record = domain_validation_option.resource_record_value
-      type   = domain_validation_option.resource_record_type
-    }
-  }
-
-  zone_id = aws_route53_zone.linc-brain-mit.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = "300"
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes = [records, name, type]
-  }
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-}
+# Aaron -- ended up having to create manually in AWS Console
+# resource "aws_route53_record" "validation" {
+#   for_each = {
+#     for domain_validation_option in aws_acm_certificate.cert.domain_validation_options : domain_validation_option.domain_name => {
+#       name   = domain_validation_option.resource_record_name
+#       record = domain_validation_option.resource_record_value
+#       type   = domain_validation_option.resource_record_type
+#     }
+#   }
+#
+#   zone_id = aws_route53_zone.linc-brain-mit.zone_id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.record]
+#   ttl     = "300"
+#
+#   lifecycle {
+#     create_before_destroy = true
+#     ignore_changes = [records, name, type]
+#   }
+# }
+#
+# resource "aws_acm_certificate_validation" "cert" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+# }
 
 resource "aws_route53_record" "email" {
   zone_id = aws_route53_zone.linc-brain-mit.zone_id
