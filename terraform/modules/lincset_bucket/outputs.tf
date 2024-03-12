b8eae5aa2a2113db4f4363482dc3d5881a5797ed
Aaron Kanzer <aaronkanzer@dhcp-10-29-225-14.dyn.mit.edu>
2024-03-12 16:16:08 -0400

    more separation

--- terraform/modules/lincset_bucket/outputs.tf
+++ terraform/modules/lincset_bucket/outputs.tf
@@ -12,3 +12,18 @@ output "bucket_arn" {
   value       = aws_s3_bucket.lincset_bucket.arn
   description = "The S3 bucket ARN."
 }
+
+output "bucket_name_us_east_2" {
+  value       = aws_s3_bucket.lincset_bucket_us_east_2.id
+  description = "The S3 bucket name."
+}
+
+output "log_bucket_name_us_east_2" {
+  value       = aws_s3_bucket.log_bucket_us_east_2.id
+  description = "The S3 log bucket name."
+}
+
+output "bucket_arn_us_east_2" {
+  value       = aws_s3_bucket.lincset_bucket_us_east_2.arn
+  description = "The S3 bucket ARN."
+}
