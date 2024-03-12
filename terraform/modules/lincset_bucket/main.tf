48d7ece16b1aa71a0d0ebf5b2b4397c18ed633e7
Aaron Kanzer <aaronkanzer@dhcp-10-29-225-14.dyn.mit.edu>
2024-03-12 16:09:07 -0400

    update linc bucket configs to us-east-2 alternative

--- terraform/modules/lincset_bucket/main.tf
+++ terraform/modules/lincset_bucket/main.tf
@@ -290,3 +290,297 @@ resource "aws_s3_bucket_lifecycle_configuration" "expire_deleted_objects" {
     status = "Enabled"
   }
 }
+
+
+data "aws_caller_identity" "sponsored_account_us_east_2" {
+  provider = aws
+}
+
+# data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "lincset_bucket_us_east_2" {
+  bucket = var.bucket_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "lincset_bucket_us_east_2" {
+  bucket = aws_s3_bucket.lincset_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "lincset_bucket_us_east_2" {
+  bucket = aws_s3_bucket.lincset_bucket.id
+
+  cors_rule {
+    allowed_origins = [
+      "*",
+    ]
+    allowed_methods = [
+      "PUT",
+      "POST",
+      "GET",
+      "DELETE",
+    ]
+    allowed_headers = [
+      "*"
+    ]
+    expose_headers = [
+      "ETag",
+    ]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_logging" "lincset_bucket_us_east_2" {
+  bucket = aws_s3_bucket.lincset_bucket.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = ""
+}
+
+resource "aws_s3_bucket_versioning" "lincset_bucket_us_east_2" {
+  count = var.versioning ? 1 : 0
+
+  bucket = aws_s3_bucket.lincset_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "lincset_bucket_us_east_2" {
+  bucket = aws_s3_bucket.lincset_bucket_us_east_2.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "lincset_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.lincset_bucket]
+
+  bucket = aws_s3_bucket.lincset_bucket_us_east_2.id
+
+  // Public access is granted via a bucket policy, not a canned ACL
+  acl = "private"
+}
+
+resource "aws_iam_user_policy" "lincset_bucket_owner_us_east_2" {
+  // The Heroku IAM user will always be in the project account
+  provider = aws.project
+
+  name = "${var.bucket_name}-ownership-policy"
+  user = var.heroku_user.user_name
+
+  policy = data.aws_iam_policy_document.lincset_bucket_owner_us_east_2.json
+}
+
+data "aws_iam_policy_document" "lincset_bucket_owner_us_east_2" {
+  version = "2008-10-17"
+
+  statement {
+    resources = [
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}",
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+    ]
+
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Delete*",
+    ]
+  }
+
+  dynamic "statement" {
+    for_each = var.allow_heroku_put_object ? [1] : []
+    content {
+
+      resources = [
+        "${aws_s3_bucket.lincset_bucket_us_east_2.arn}",
+        "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+      ]
+
+      actions = ["s3:PutObject"]
+    }
+  }
+
+  statement {
+    resources = [
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}",
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+    ]
+
+    actions = ["s3:*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "lincset_bucket_policy_us_east_2" {
+  provider = aws
+
+  bucket = aws_s3_bucket.lincset_bucket_us_east_2.id
+  policy = data.aws_iam_policy_document.lincset_bucket_policy_us_east_2.json
+}
+
+data "aws_iam_policy_document" "lincset_bucket_policy_us_east_2" {
+  version = "2008-10-17"
+
+  dynamic "statement" {
+    for_each = var.public ? [1] : []
+
+    content {
+      resources = [
+        "${aws_s3_bucket.lincset_bucket_us_east_2.arn}",
+        "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+      ]
+
+      actions = [
+        "s3:Get*",
+        "s3:List*",
+      ]
+
+      principals {
+        identifiers = ["*"]
+        type        = "*"
+      }
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.allow_cross_account_heroku_put_object ? [1] : []
+
+    content {
+      sid = "S3PolicyStmt-DO-NOT-MODIFY-1569973164923"
+      principals {
+        identifiers = ["s3.amazonaws.com"]
+        type        = "Service"
+      }
+      actions = [
+        "s3:PutObject",
+      ]
+      resources = [
+        "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+      ]
+      condition {
+        test     = "StringEquals"
+        variable = "aws:SourceAccount"
+        values   = [data.aws_caller_identity.sponsored_account.account_id]
+      }
+      condition {
+        test     = "StringEquals"
+        variable = "s3:x-amz-acl"
+        values   = ["bucket-owner-full-control"]
+      }
+      condition {
+        test     = "ArnLike"
+        variable = "aws:SourceArn"
+        values   = [aws_s3_bucket.lincset_bucket_us_east_2.arn]
+      }
+    }
+  }
+
+  statement {
+    resources = [
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}",
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+    ]
+
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Delete*",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.heroku_user.arn]
+    }
+  }
+
+  statement {
+    resources = [
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}",
+      "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*",
+    ]
+
+    actions = ["s3:*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.heroku_user.arn]
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.trailing_delete ? [1] : []
+
+    content {
+      sid = "PreventDeletionOfObjectVersions"
+
+      resources = [
+        "${aws_s3_bucket.lincset_bucket_us_east_2.arn}/*"
+      ]
+
+      actions = [
+        "s3:DeleteObjectVersion",
+      ]
+
+      effect = "Deny"
+
+      principals {
+        identifiers = ["*"]
+        type        = "*"
+      }
+    }
+  }
+}
+
+
+# S3 lifecycle policy that permanently deletes objects with delete markers
+# after 30 days.
+resource "aws_s3_bucket_lifecycle_configuration" "expire_deleted_objects_us_east_2" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.lincset_bucket_us_east_2]
+
+  count = var.trailing_delete ? 1 : 0
+
+  bucket = aws_s3_bucket.lincset_bucket_us_east_2.id
+
+  # Based on https://docs.aws.amazon.com/AmazonS3/latest/userguide/lifecycle-configuration-examples.html#lifecycle-config-conceptual-ex7
+  rule {
+    id = "ExpireOldDeleteMarkers"
+    filter {}
+
+    # Expire objects with delete markers after 30 days
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+
+    # Also delete any delete markers associated with the expired object
+    expiration {
+      expired_object_delete_marker = true
+    }
+
+    status = "Enabled"
+  }
+}
--- terraform/sponsored_iam.tf
+++ terraform/sponsored_iam.tf
@@ -42,3 +42,51 @@ data "aws_iam_policy_document" "sponsored_writers" {
     ]
   }
 }
+
+resource "aws_iam_group" "sponsored_writers_us_east_2" {
+  provider = aws.target
+
+  name = "writers"
+}
+
+resource "aws_iam_group_policy" "sponsored_writers_us_east_2" {
+  provider = aws.target
+
+  name   = "bucket-write"
+  group  = aws_iam_group.sponsored_writers_us_east_2.name
+  policy = data.aws_iam_policy_document.sponsored_writers_us_east_2.json
+}
+
+data "aws_iam_policy_document" "sponsored_writers_us_east_2" {
+  version = "2012-10-17"
+  statement {
+    sid = "VisualEditor0"
+    actions = [
+      "s3:DeleteObjectTagging",
+      "s3:ListBucketByTags",
+      "s3:ListBucketMultipartUploads",
+      "s3:GetBucketTagging",
+      "s3:ListBucketVersions",
+      "s3:PutObjectVersionTagging",
+      "s3:ListBucket",
+      "s3:DeleteObjectVersionTagging",
+      "s3:GetBucketVersioning",
+      "s3:GetObjectVersionTorrent",
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:PutBucketTagging",
+      "s3:GetObjectTagging",
+      "s3:PutObjectTagging",
+      "s3:DeleteObject",
+      "s3:GetBucketLocation",
+      "s3:GetObjectVersion",
+    ]
+    resources = [
+      "${module.sponsored_lincset_bucket_us_east_2.bucket_arn}/*",
+      module.sponsored_lincset_bucket_us_east_2.bucket_arn,
+    ]
+  }
+}
+
+
+
